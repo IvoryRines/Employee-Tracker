e1e37b13c738f2a46d534a73173c2dd093e34fb3
EyeVorE <subplot_05tracer@icloud.com>
2024-05-17 01:07:14 -0500

    added comments to db.js

--- utils/db.js
+++ utils/db.js
@@ -1,14 +1,13 @@
-//Connect to db
-
+// Import the Pool class from the pg module to manage PostgreSQL connections
 const { Pool } = require('pg');
 
-
+// Create a new Pool instance to manage database connections
 const db = new Pool({
-  host: "localhost",
-  user: "postgres",
-  password: "pwd123",
-  database: "employees_db",
+  host: "localhost",       
+  user: "postgres",        
+  password: "pwd123",      
+  database: "employees_db" 
 });
 
-
+// Export the db instance to be used in other modules
 module.exports = { db };
